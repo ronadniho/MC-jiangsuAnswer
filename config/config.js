554e4d79b7d2935121012669cac7005a0a1a6217
ronadniho <463329429@qq.com>
2018-07-13 10:52:30 +0800

    first

---
+++ config/config.js
@@ -0,0 +1,6 @@
+exports.config =  {
+  "testUrl": "http://localhost:9988/",
+  "productUrl":"http://localhost:9988/",
+  // "productUrls":"http://localhost:9988/JSSCZTOnlineAnswer/",
+  "productUrls":"http://localhost:9999/",
+}
